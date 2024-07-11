2255a0003eeb7abb6670191c15a142e7e5dc15ca
Hero Protagonist <heroprotagonist32@gmail.com>
2024-07-10 21:10:54 -0400

    add auto-copy watcher to the testing vault

---
+++ scripts/copier.mjs
@@ -0,0 +1,21 @@
+import { watch, copyFileSync } from "fs";
+import path from "path";
+
+const pluginFolder = path.resolve(
+	"../Playground/.obsidian/plugins/canvas-element-link",
+);
+const vaultPaths = {
+	mainjs: path.join(pluginFolder, "main.js"),
+	styles: path.join(pluginFolder, "styles.css"),
+	manifest: path.join(pluginFolder, "manifest.json"),
+};
+
+watch("main.js", (changeType, fileName) => {
+	copyFileSync(fileName, vaultPaths.mainjs);
+});
+watch("styles.css", (changeType, fileName) => {
+	copyFileSync(fileName, vaultPaths.styles);
+});
+watch("manifest.json", (changeType, fileName) => {
+	copyFileSync(fileName, vaultPaths.mainjs);
+});
