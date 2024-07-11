64b6a5bc652e76f94002476dd1d57ce11a2bdd35
Hero Protagonist <heroprotagonist32@gmail.com>
2024-07-10 21:22:26 -0400

    add manifest to copier

--- scripts/copier.mjs
+++ scripts/copier.mjs
@@ -2,7 +2,7 @@ import { watch, copyFileSync } from "fs";
 import path from "path";
 
 const pluginFolder = path.resolve(
-	"../Playground/.obsidian/plugins/canvas-element-link",
+	"../Playground/.obsidian/plugins/canvas-element-link"
 );
 const vaultPaths = {
 	mainjs: path.join(pluginFolder, "main.js"),
@@ -17,5 +17,5 @@ watch("styles.css", (changeType, fileName) => {
 	copyFileSync(fileName, vaultPaths.styles);
 });
 watch("manifest.json", (changeType, fileName) => {
-	copyFileSync(fileName, vaultPaths.mainjs);
+	copyFileSync(fileName, vaultPaths.manifest);
 });
